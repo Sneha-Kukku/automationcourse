7c3bec27bb488b707aa5d5057b4c2a2ff9df76a5
user <user@DESKTOP-UP2Q5NF>
2025-09-08 19:36:41 +0530

    commit

--- AutomationCourse/src/test/java/seleniumBasics/WebElementCommands.java
+++ AutomationCourse/src/test/java/seleniumBasics/WebElementCommands.java
@@ -1,7 +1,15 @@
 package seleniumBasics;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WebElementCommands extends Base {
 	public void handlingElements()
@@ -12,6 +20,11 @@ public class WebElementCommands extends Base {
 		msgBox.sendKeys("sneha");
 		//click
 		WebElement showButton=driver.findElement(By.xpath("//button[@id='button-one']"));
+		WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.elementToBeClickable(showButton));
+		Wait<WebDriver>fluent=new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(5)).pollingEvery(Duration.ofSeconds(2)).ignoring(NoSuchElementException.class);
+		fluent.until(ExpectedConditions.elementToBeClickable(showButton));
+				
 		showButton.click();
 		//isDisplayed
 		System.out.println(showButton.isDisplayed());
